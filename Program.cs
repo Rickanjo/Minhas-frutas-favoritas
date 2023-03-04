8a14c659dba3b1a600640144e6d602b023a50b76
Rick Arcanjo <ricardo.cassettari07@gmail.com>
2023-03-03 22:05:57 -0300

    Execícios Resolvido

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--- Minhas Frutas Favoritas ---");
+Console.WriteLine("1- Morango");
+Console.WriteLine("2- Uva");
+Console.WriteLine("3- Banana");
+Console.WriteLine("4- Maçã");
+Console.WriteLine("5- Abacaxi");
+
